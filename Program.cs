0d68b9182f42523a5ccff14ab0cfb4a5f3d1d7cb
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-01 22:21:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml;
+
+namespace XMLValuesInConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument document = new XmlDocument();
+            document.Load("NumbersXml.xml");
+            foreach (XmlNode item in document)
+            {
+                foreach (XmlNode item1 in item.ChildNodes)
+                {
+                    Console.Write(item1.InnerText);
+                }
+            }
+            Console.Read();
+        }
+    }
+}
